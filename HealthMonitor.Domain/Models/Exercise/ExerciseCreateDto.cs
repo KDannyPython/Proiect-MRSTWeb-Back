a29c2af45020cb3dc761407c95518a40cfbd60e6
sanea <sasagherciu22@gmail.com>
2026-04-01 20:12:46 +0300

    functionare CRUD complet si rework la entitati

--- HealthMonitor.Domain/Models/Exercise/ExerciseCreateDto.cs
+++ HealthMonitor.Domain/Models/Exercise/ExerciseCreateDto.cs
@@ -3,8 +3,6 @@ namespace HealthMonitor.Domain.Models.Exercise
     public class ExerciseCreateDto
     {
         public string Name { get; set; }
-        public int Sets { get; set; }
-        public int Reps { get; set; }
-        public float Weight { get; set; }
+        public string? MuscleTarget { get; set; }
     }
 }
