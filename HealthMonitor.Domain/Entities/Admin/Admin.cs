4357bfe52be8fb4d4904f75552c9b28f13c4ac7e
Maximilian Jumiga <myaconst99@gmail.com>
2026-04-03 01:18:17 +0300

    Adaugare adnotari si migrare noua

--- HealthMonitor.Domain/Entities/Admin/Admin.cs
+++ HealthMonitor.Domain/Entities/Admin/Admin.cs
@@ -1,13 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthMonitor.Domain.Entities.Admin
 {
     public class Admin
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
 
+        [Required(ErrorMessage = "Numele adminului este obligatoriu.")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email-ul adminului este obligatoriu.")]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
     }
 }
--- HealthMonitor.Domain/Entities/User/UserEntity.cs
+++ HealthMonitor.Domain/Entities/User/UserEntity.cs
@@ -1,18 +1,38 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace HealthMonitor.Domain.Entities.User
 {
     public class UserEntity
     {
+        [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Gender { get; set; }
-        public int Age { get; set; }
-        public int Height { get; set; }
-        public int Weight { get; set; }
-        public string Goal { get; set; } // lose weight, gain muscle, maintain weight 
 
+        [Required(ErrorMessage = "Numele este obligatoriu.")]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email-ul este obligatoriu.")]
+        [EmailAddress(ErrorMessage = "Ai introdus un format greșit de email.")]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Parola este obligatorie.")]
+        public string PasswordHash { get; set; }
+
+        [MaxLength(20)]
+        public string Gender { get; set; }
+        
+        [Range(1, 120, ErrorMessage = "Vârsta trebuie să fie între 1 și 120 de ani.")]
+        public int Age { get; set; }
+        
+        [Range(30, 250, ErrorMessage = "Înălțimea (cm) este incorectă.")]
+        public int Height { get; set; }
+        
+        [Range(10, 300, ErrorMessage = "Greutatea (kg) este incorectă.")]
+        public int Weight { get; set; }
+
+        [MaxLength(50)]
+        public string Goal { get; set; } 
     }
 }
