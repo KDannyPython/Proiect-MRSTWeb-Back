1fda5e6d9d6ba7cdd3c2f32e090925d09badc86c
sanea <sasagherciu22@gmail.com>
2026-05-14 15:47:17 +0300

    lista de exercitii cu fielduri respective completate predefinite

--- HealthMonitor.Domain/Models/Exercise/ExerciseCreateDto.cs
+++ HealthMonitor.Domain/Models/Exercise/ExerciseCreateDto.cs
@@ -1,8 +1,13 @@
+using HealthMonitor.Domain.Entities.Exercise;
+
 namespace HealthMonitor.Domain.Models.Exercise
 {
     public class ExerciseCreateDto
     {
         public string Name { get; set; }
-        public string? MuscleTarget { get; set; }
+        public MuscleGroup PrimaryMuscleGroup { get; set; }
+        public string? SecondaryMuscleGroup { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public FatigueCost FatigueCost { get; set; }
     }
 }
