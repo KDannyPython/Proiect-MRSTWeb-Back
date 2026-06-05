e2803e58b2d0d06b264b5fd4377098d77535c37b
Danny K <dk7999357@gmail.com>
2026-06-05 19:51:59 +0300

    Ascundere JwtSecretKey in UserSecrets

--- HealthMonitor.BusinessLayer/Core/JwtSettings.cs
+++ HealthMonitor.BusinessLayer/Core/JwtSettings.cs
@@ -4,7 +4,6 @@
     {
         public const string Issuer = "eHealthMonitorApi";
         public const string Audience = "eHealthMonitorClients";
-        public const string SecretKey = "AlabalaPortocalaCineMi_aFuratBanana";
         public const int ExpireMinutes = 60;
     }
 }
